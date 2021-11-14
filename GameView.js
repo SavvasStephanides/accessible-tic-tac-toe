b81835a26298ead714fb57e40d71c4021c31758e
Savvas <savvas@Savvass-MacBook-Pro.local>
2021-11-14 15:23:16 +0000

    Use createElement function for displayBoard @

--- GameView.js
+++ GameView.js
@@ -4,15 +4,33 @@ function GameView(){
 
     function displayBoard(){
         let boardElement = document.querySelector("#board")
-        
-        boardElement.innerHTML = game.board.map((tile) => 
-            `<div class="tile" horizontal="${tile.horizontalPosition}" vertical="${tile.verticalPosition}" onclick="gameView.tileClickEvent(event)">
-                <div class="tile-flag">
-                    ${tile.flag}
-                    <button class="action-button">Add flag to ${tile.horizontalPosition} ${tile.verticalPosition}</button>
-                </div>
-            </div>`)
-            .join("")
+        boardElement.innerHTML = ""
+
+        game.board.forEach((tile) => {
+            let tileElement = document.createElement("div")
+            tileElement.classList.add("tile")
+            tileElement.setAttribute("horizontal", tile.horizontalPosition)
+            tileElement.setAttribute("vertical", tile.verticalPosition)
+
+            let flagElement = document.createElement("div")
+            flagElement.classList.add("tile-flag")
+            flagElement.innerHTML = tile.flag
+
+            tileElement.addEventListener("click", () => {
+                game.playNextRound({
+                    horizontal: tile.horizontalPosition,
+                    vertical: tile.verticalPosition
+                })
+                displayBoard()
+
+                if(game.gameHasEnded()){
+                    alert(`Game over!`)
+                }
+            })
+
+            tileElement.appendChild(flagElement)
+            boardElement.appendChild(tileElement)
+        })
     }
 
     function tileClickEvent(e){

73287594c3d3940113792c805ffbd4b6a9eae774
Savvas <savvas@savvass-mbp.home>
2021-11-14 19:20:51 +0000

    Keyboard accessibility

--- GameView.js
+++ GameView.js
@@ -11,6 +11,18 @@ function GameView(){
             tileElement.setAttribute("horizontal", tile.horizontalPosition)
             tileElement.setAttribute("vertical", tile.verticalPosition)
 
+            if(tile.flag === ""){
+                let buttonElement = document.createElement("button")
+                buttonElement.classList.add("action-button")
+                buttonElement.innerHTML = `Add ${game.flagTurn} flag to ${tile.verticalPosition} ${tile.horizontalPosition}`
+                
+                buttonElement.addEventListener("click", () => {
+                    tileClickEvent(tile)
+                })
+
+                tileElement.appendChild(buttonElement)
+            }
+
             let flagElement = document.createElement("div")
             flagElement.classList.add("tile-flag")
             flagElement.innerHTML = tile.flag
@@ -29,7 +41,7 @@ function GameView(){
             horizontal: tile.horizontalPosition,
             vertical: tile.verticalPosition
         })
-        
+
         displayBoard()
 
         if(game.gameHasEnded()){
